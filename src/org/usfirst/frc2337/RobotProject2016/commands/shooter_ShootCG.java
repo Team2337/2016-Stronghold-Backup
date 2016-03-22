a0cd14d3fcd751f336a365fe4e81b6abcef80427
slynch05 <slynch05@yahoo.com>
2016-03-22 19:33:02 -0400

    updated Shoot command to retract prior to releasing wrist.

--- src/org/usfirst/frc2337/RobotProject2016/commands/shooter_ShootCG.java
+++ src/org/usfirst/frc2337/RobotProject2016/commands/shooter_ShootCG.java
@@ -24,14 +24,15 @@ public class shooter_ShootCG extends CommandGroup {
     		
     	addSequential(new shooter_Shoot());
     	//addSequential(new datePrint());
-    	addSequential(new auton_Wait(0.8));  				//.......adjust or remove???
+    	addSequential(new auton_Wait(0.5));  				//.......adjust or remove???
 
     	//addSequential(new shooterRetractor_turnOnBrakeMode());
     	
     	addSequential(new intake_DoNothing());
-    	addSequential(new intakeWrist_Retract());
     	addSequential(new shooter_UnShoot());
     	addSequential(new shooterRetract_Prime());
+    	addSequential(new intakeWrist_Retract());
+
     	//addSequential(new auton_Wait(0.5)); 
     	//addSequential(new shooterRetract_Prep());
     	
